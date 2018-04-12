b9f76e5f76bb52253c53abf24bcbafd77a86445d
nalmujel <38320692+nalmujel@users.noreply.github.com>
2018-04-12 16:24:26 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,14 +2,15 @@
 package flowerproject;
 
 import  java.io.*;
-
+import java.util.Scanner;
 public class Customer {
+    Scanner in = new Scanner(System.in);
     private long id;
     private String name;
     private int phonenumber;
     private String mail;
     private String Adress;
-
+  
 
         public Customer(){
             id = 0;
@@ -61,6 +62,34 @@ public class Customer {
     @Override
     public String toString() {
         return "Customer{" + "id=" + id + ", name=" + name + ", phone=" + phonenumber + ", mail=" + mail + '}';
+    }public void PaymentMethod(){
+           int choice=0;
+            int ccv=0;
+            int visa=0;
+            String NameV="";
+            int time=0;
+            System.out.println("Please Choose The Payment Method That you'd Like for your Order ");
+            System.out.println("1: Visa");
+            System.out.println("2:Cash Delievry");
+            
+            choice=in.nextInt();
+            switch(choice){
+                case 1:
+                    System.out.println("Enter the Visa Number");
+                    visa=in.nextInt();
+                    System.out.println("Enter CCV Number");
+            ccv=in.nextInt();
+                    System.out.println("Enter The Name of The Visa");
+                    NameV=in.next();
+            break;
+                case 2: 
+                        System.out.println("Enter Your Adress");
+                    Adress=in.next();
+                    break;
+                default:
+                    System.out.println("Sorry but The Payment Method That You have choosed Isn't Available At our Shop");
+            }
+    
     }
     
 
